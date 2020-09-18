17cfb9f198047c93a14c70657cc6252c5190d701
PieterH <huybrechtspieter@gmail.com>
2020-09-18 13:08:25 +0200

    Show calendar

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,15 +3,50 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {
+  CalendarDateFormatter,
+  CalendarModule,
+  CalendarMomentDateFormatter,
+  DateAdapter,
+  MOMENT
+} from "angular-calendar";
+import {adapterFactory} from "angular-calendar/date-adapters/moment";
+// @ts-ignore
+import moment from 'moment';
+import {CommonModule} from "@angular/common";
+
+export function momentAdapterFactory() {
+  return adapterFactory(moment);
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    CommonModule,
+    BrowserModule, HttpClientModule,
+    BrowserAnimationsModule,
+
+    CalendarModule.forRoot(
+      {
+      provide: DateAdapter,
+      useFactory: momentAdapterFactory,
+    },
+      {
+        dateFormatter: {
+          provide: CalendarDateFormatter,
+          useClass: CalendarMomentDateFormatter,
+        },
+      }),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MOMENT,
+      useValue: moment,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
