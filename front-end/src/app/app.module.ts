d2316881a1a3fe3f151121651b3a5ef2b45b7348
PieterH <huybrechtspieter@gmail.com>
2020-09-18 23:55:21 +0200

    remove ng calendar dependencies

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,26 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {
-  CalendarDateFormatter,
-  CalendarModule,
-  CalendarMomentDateFormatter,
-  DateAdapter,
-  MOMENT
-} from "angular-calendar";
-import {adapterFactory} from "angular-calendar/date-adapters/moment";
-// @ts-ignore
-import moment from 'moment';
+
 import {CommonModule} from "@angular/common";
 import {DayCellComponent} from "./components/DayCell/day-cell.component";
 import {CalendarService} from "./services/calendar.service";
-
-export function momentAdapterFactory() {
-  return adapterFactory(moment);
-}
 
 @NgModule({
   declarations: [
@@ -29,28 +16,14 @@ export function momentAdapterFactory() {
   ],
   imports: [
     CommonModule,
-    BrowserModule, HttpClientModule,
+    BrowserModule,
+    HttpClientModule,
     BrowserAnimationsModule,
-
-    CalendarModule.forRoot(
-      {
-      provide: DateAdapter,
-      useFactory: momentAdapterFactory,
-    },
-      {
-        dateFormatter: {
-          provide: CalendarDateFormatter,
-          useClass: CalendarMomentDateFormatter,
-        },
-      }),
   ],
   providers: [
-    {
-      provide: MOMENT,
-      useValue: moment,
-    },
     CalendarService
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
