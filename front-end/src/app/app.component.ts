679348cb00f2b2b2efa5d070ea0c96b6507805b5
PieterH <huybrechtspieter@gmail.com>
2020-09-19 20:26:10 +0200

    Add ability to navigate between months

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {CalendarService} from "./services/calendar.service";
 import {Observable} from "rxjs";
-import {Calendar} from "./domain/calendar.model";
+import {Calendar, Month} from "./domain/calendar.model";
+import {tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -11,10 +12,57 @@ import {Calendar} from "./domain/calendar.model";
 export class AppComponent implements OnInit {
   calendar$: Observable<Calendar>;
 
+  currentMonth: Month;
+
+  private calendar: Calendar;
+  private currentMonthIndex: number;
+
   constructor(private calendarService: CalendarService) {
   }
 
   ngOnInit(): void {
-    this.calendar$ = this.calendarService.getCalendar();
+    this.calendar$ = this.calendarService.getCalendar().pipe(
+      tap((calendar: Calendar) => this.calendar = calendar),
+      tap(() => this.showCurrentMonth())
+    );
+  }
+
+  private setCurrentMonthIndex(monthIndex: number) {
+    if (monthIndex < 0) {
+      this.currentMonthIndex = 0;
+    } else if (monthIndex >= this.calendar.months.length) {
+      this.currentMonthIndex = this.calendar.months.length - 1;
+    } else {
+      this.currentMonthIndex = monthIndex;
+    }
+  }
+
+  showPreviousMonth() {
+    this.setCurrentMonthIndex(this.currentMonthIndex - 1);
+    this.showMonthWithIndex(this.currentMonthIndex);
+  }
+
+  showCurrentMonth() {
+    this.setCurrentMonthIndex(this.calculateCurrentMonthIndex());
+    this.showMonthWithIndex(this.currentMonthIndex);
+  }
+
+  showNextMonth() {
+    this.setCurrentMonthIndex(this.currentMonthIndex + 1);
+    this.showMonthWithIndex(this.currentMonthIndex);
+  }
+
+  showMonthWithIndex(monthIndex: number) {
+    this.currentMonth = this.calendar.months[monthIndex];
+  }
+
+  private calculateCurrentMonthIndex(): number {
+    return this.calendar.months
+      .indexOf(this.findCurrentMonth());
+  }
+
+  private findCurrentMonth() {
+    return this.calendar.months
+      .find(month => month.year === new Date().getFullYear().toString() && month.monthOfYear === new Date().getMonth());
   }
 }
