a855a2d9f42dac9d11b5b84c52bfb47d8ccf5201
PieterH <huybrechtspieter@gmail.com>
2020-09-21 20:05:18 +0200

    paced html and css of app component in one file

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -8,10 +8,12 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {CommonModule} from "@angular/common";
 import {DayCellComponent} from "./components/DayCell/day-cell.component";
 import {CalendarService} from "./services/calendar.service";
+import {CalendarComponent} from "./components/calendar.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    CalendarComponent,
     DayCellComponent
   ],
   imports: [
---
+++ front-end/src/app/components/calendar.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'rdc-calendar',
+  template: ``
+})
+export class CalendarComponent {
+
+}
