3cc4c18a6ac545c51495cb2436140e4e13207f08
Jooones <jonathanV@bitbucket.org>
2020-10-08 18:36:57 +0200

    remove commented code

--- front-end/src/app/components/DayCell/day-cell.component.ts
+++ front-end/src/app/components/DayCell/day-cell.component.ts
@@ -27,82 +27,8 @@ import { Day, DayType, Month } from "../../domain/calendar.model";
         <div *ngIf="isPrdInstallDay(day.dayType)" class="prd-install-day">PRD INSTALL {{day.stgVersion}}</div> <!-- stgVersion.. I know.. ¯\_(ツ)_/¯ -->
       </div>
     </div>
-
-    <!--<div *ngIf="day" class='day-cell' [ngClass]="{'weekend-day': isWeekendDay(day.dayOfWeek), 'stg-install-day': isStgInstallDay(day.dayType), 'slight-border': true, 'today': isToday}">
-        {{day.dayOfMonth}}<br/>
-        <div class="box-height">
-          <div *ngIf="isNewSprintDay(day.dayType)" class="new-sprint-day">START SPRINT {{day.developVersion.substr(3)}}</div>
-          <div *ngIf="isStgInstallDay(day.dayType)" class="text-center">STG INSTALL {{day.stgVersion}}</div>
-        </div>
-        <div class="d-inline-block slight-padding-top-bottom">
-          dev: {{day.developVersion}}<br/>
-          rc: {{day.rcVersion}}<br/>
-        </div>
-        <div class="float-right slight-padding-top-bottom slightly-faded-text">
-          stg: {{day.stgVersion}}<br/>
-          prd: {{day.prdVersion}}<br/>
-        </div>
-        <div class="box-height">
-          <div *ngIf="isPrdInstallDay(day.dayType)" class="prd-install-day">PRD INSTALL {{day.stgVersion}}</div>-->
-    <!-- stgVersion.. I know.. ¯_(ツ)_/¯ -->
-    <!-- </div>
-    </div> -->
   `,
   styleUrls: ["./day-cell.component.css"]
-//   styles: [`
-//   .day-cell {
-//     height:100%;
-//   }
-
-//   .new-sprint-day {
-//     background-color: lightgreen;
-//     text-align: center;
-//   }
-
-//   .stg-install-day {
-//     background-color: moccasin;
-//   }
-
-//   .prd-install-day {
-//     background-color: pink;
-//     text-align: center;
-//   }
-
-//   .slight-border {
-//     padding: 5px;
-//   }
-
-//   .today {
-//     border: solid 3px lightskyblue;
-//     padding: 2px !important;
-//   }
-
-//   .normal-day {
-//   }
-
-//   .wut-day {
-//     background-color: darkred;
-//   }
-
-//   .weekend-day {
-//     background-color: lightgrey;
-//     margin: 0;
-//   }
-
-//   .box-height {
-//     height: 24px
-//   }
-
-//   .slight-padding-top-bottom {
-//     padding-top: 5px;
-//     padding-bottom: 5px;
-//   }
-
-//   .slightly-faded-text {
-//     color: gray;
-//   }
-// `
-// ]
 })
 export class DayCellComponent implements OnInit {
   @Input()
