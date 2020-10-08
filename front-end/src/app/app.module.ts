8357bad2fc28c0c4e330164ebf7422564b826bf0
Jooones <jonathanV@bitbucket.org>
2020-10-08 21:37:18 +0200

    add ng bootstrap, add tooltip to days and remove obsolete br's

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -9,6 +9,7 @@ import {CommonModule} from "@angular/common";
 import {DayCellComponent} from "./components/DayCell/day-cell.component";
 import {CalendarService} from "./services/calendar.service";
 import {CalendarMonthComponent} from "./components/calendar-month.component";
+import {NgbAlertModule, NgbTooltipModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,8 @@ import {CalendarMonthComponent} from "./components/calendar-month.component";
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    NgbAlertModule,
+    NgbTooltipModule
   ],
   providers: [
     CalendarService
--- front-end/src/app/components/DayCell/day-cell.component.ts
+++ front-end/src/app/components/DayCell/day-cell.component.ts
@@ -4,6 +4,10 @@ import { Day, DayType, Month } from "../../domain/calendar.model";
 @Component({
   selector: "rdc-day-cell-component",
   template: `
+    <ng-template #tipContent>
+      DEV: {{day.developVersion}} RC: {{day.rcVersion}}<br/>
+      STG: {{day.stgVersion}} PRD: {{day.prdVersion}}
+    </ng-template>
     <div
       *ngIf="day"
       class="day-cell"
@@ -12,16 +16,17 @@ import { Day, DayType, Month } from "../../domain/calendar.model";
         'stg-install-day': isStgInstallDay(day.dayType),
         today: isToday
       }"
+      [ngbTooltip]="tipContent"
     >
       <div class="day-item">{{ day.dayOfMonth }}</div>
       <div class="installation-header">
           <div *ngIf="isNewSprintDay(day.dayType)" class="new-sprint-day">START SPRINT {{day.developVersion.substr(3)}}</div>
           <div *ngIf="isStgInstallDay(day.dayType)" class="text-center">STG INSTALL {{day.stgVersion}}</div>
       </div>
-      <div class="dev-version">dev: {{day.developVersion}}<br/></div>
-      <div class="rc-version">rc: {{day.rcVersion}}<br/></div>
-      <div class="stg-version float-right">stg: {{day.stgVersion}}<br/></div>
-      <div class="prd-version float-right">prd: {{day.prdVersion}}<br/></div>
+      <div class="dev-version">dev: {{day.developVersion}}</div>
+      <div class="rc-version">rc: {{day.rcVersion}}</div>
+      <div class="stg-version float-right">stg: {{day.stgVersion}}</div>
+      <div class="prd-version float-right">prd: {{day.prdVersion}}</div>
       <div class="installation-footer">
         <div *ngIf="isPrdInstallDay(day.dayType)" class="prd-install-day">PRD INSTALL {{day.stgVersion}}</div> <!-- stgVersion.. I know.. ¯\_(ツ)_/¯ -->
       </div>
