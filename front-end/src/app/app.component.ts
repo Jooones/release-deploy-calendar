f71d0371b66a35d3a02f4f9a9b95e4c14a323f45
David Op de Beeck <davidopdebeeck@hotmail.com>
2020-10-21 22:34:55 +0200

    Added settings button and added breakpoints to header

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -6,7 +6,7 @@ import {Calendar, Month} from "./domain/calendar.model";
   selector: 'app-root',
   template: `
     <section *ngIf="calendar" class="flex flex-col h-screen">
-      <section class="flex items-center justify-center p-2 text-xl text-gray-700 space-x-2">
+      <section class="flex items-center xs:justify-start sm:justify-center p-2 text-xl text-gray-700 space-x-2">
         <button class="bg-gray-100 hover:bg-gray-200 p-2 rounded-full focus:outline-none"
                 (click)="showPreviousMonth()">
           <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -24,18 +24,35 @@ import {Calendar, Month} from "./domain/calendar.model";
         </button>
 
         <section class="absolute right-0 flex space-x-1 p-1 mr-1 select-none text-sm">
-          <button class="text-white bg-blue-500 hover:bg-blue-600 border border-blue-700 p-2 rounded focus:outline-none"
+          <button class="bg-white hover:bg-gray-100 border border-gray-200 p-2 rounded shadow-xs focus:outline-none"
                   (click)="showCurrentMonth()">
             <span>{{ currentMonthName }} {{ currentMonth.year }}</span>
           </button>
-          <label class="inline-flex items-center p-2 rounded space-x-1">
-            <input id="devRcVersions" type="checkbox" [(ngModel)]="devRcVersions">
-            <span>dev and rc versions</span>
-          </label>
-          <label class="inline-flex items-center p-2 rounded space-x-1">
-            <input id="stgPrdVersions" type="checkbox" [(ngModel)]="stgPrdVersions">
-            <span>stg and prd versions</span>
-          </label>
+          <section class="relative">
+            <button class="bg-gray-100 hover:bg-gray-200 p-2 rounded-full focus:outline-none"
+                    (click)="toggleSettings()">
+              <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                   xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path>
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+              </svg>
+            </button>
+            <section *ngIf="settingsShown"
+                     class="origin-top-right absolute right-0 mt-1">
+              <section class="flex flex-col bg-white shadow-xs rounded divide-y divide-gray-200">
+                <label class="inline-flex items-center p-2 space-x-1 w-40">
+                  <input type="checkbox" [(ngModel)]="devRcVersions">
+                  <span>dev and rc versions</span>
+                </label>
+                <label class="inline-flex items-center p-2 space-x-1">
+                  <input type="checkbox" [(ngModel)]="stgPrdVersions">
+                  <span>stg and prd versions</span>
+                </label>
+              </section>
+            </section>
+          </section>
         </section>
       </section>
 
@@ -61,6 +78,8 @@ export class AppComponent implements OnInit {
   stgPrdVersions: boolean = false;
 
   calendar: Calendar;
+
+  settingsShown: boolean = false;
 
   constructor(private calendarService: CalendarService) {
   }
@@ -121,5 +140,9 @@ export class AppComponent implements OnInit {
       "July", "August", "September", "October", "November", "December"
     ];
     return monthNames[monthIndex];
+  }
+
+  toggleSettings() {
+    this.settingsShown = !this.settingsShown;
   }
 }
